768e376bcb582c498207f077b387526ed4685b96
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-15 00:55:02 +0530

    [Dipti] added code for swapping nibble and show power of 2 or not

---
+++ NunitTesting_CS/SwapNibble.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting_CS
+{
+    class SwapNibble
+    {
+        public static void SwapNibbles() 
+        {
+            Console.WriteLine("Enter a value");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int result = ((x & 0x0F) << 4 | (x & 0xF0) >> 4); 
+            Console.WriteLine("After swapping a bits " + result);
+            //turnary operator use-> method(variable) ? is like if condition,: is like else .
+            Console.WriteLine(isPowerOfTwo(result) ? "Number is power of 2" : "Number is not a power of 2");    
+        }
+
+        // Method to check if result is power of 2
+        public static bool isPowerOfTwo(int result)
+        {
+            if (result==0)
+            {
+                return false;
+            }
+            while(result != 1)
+            {
+                if(result %2 != 0)
+                {
+                    return false;
+                }
+                result = result / 2;
+            }
+            return true;
+        }
+    }
+}
